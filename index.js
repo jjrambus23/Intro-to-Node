8a6184326bc2080763bd5a29b6c402e31fb5c867
jjrambus23 <jjrambus23@gmail.com>
2023-04-11 18:03:00 -0400

    npm challenge installing supervillain names

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
 var superheroes = require("superheroes");
+var supervillians = require("supervillains")
 
 var mySuperHero = superheroes.random();
+var myVillain = supervillians.random();
 
-console.log(mySuperHero)
+console.log(mySuperHero);
+console.log(myVillain);
